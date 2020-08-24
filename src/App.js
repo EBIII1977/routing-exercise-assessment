0fae5e427f34299f0a8f6766bebcbafd17f67f32
Eugene Bridges III <ebiii@hotmail.com>
2020-08-24 15:57:47 -0500

    assessment complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import "./App.css";
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Navigation from "./components/navigation/Navigation";
+import PageNotFound from "./components/pageNotFound/PageNotFound"
 
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Route
-        exact
-        path="/"
-        render={(props) => <Welcome {...props} name="Eugene" />}
-      />
-      <Route path="/clock" component={Clock} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/welcome" component={Welcome} />
+      <Switch>
+
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Eugene" />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+        <Route component={PageNotFound} />
+
+      </Switch>
+
     </div>
   );
 }
---
+++ src/components/pageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFound(props) {
+  return (
+    <div>
+      <h3>
+        Page not found
+      </h3>
+    </div>)
+}
+
+export default PageNotFound;
